fe3d07833717e23e16b0f325e89eb3df712135f2
Josué Moreau <jsmr14.12@gmail.com>
2021-03-08 03:36:30 +0100

    commentaires main.ml

--- main.ml
+++ main.ml
@@ -16,19 +16,19 @@ let () =
         let p = lb.lex_curr_p in
         printf "Error : line %d, column %d" p.pos_lnum (p.pos_cnum - p.pos_bol);
         exit 1 in
-    List.iter (fun t -> print_type_def t) tlist;
-    printf "@.";
+    (* List.iter (fun t -> print_type_def t) tlist;
+     * printf "@."; *)
     List.iter (fun t ->
         let _ = compile_label t.label in ()) tlist;
     let ta = compile_types tlist Sys.argv.(3) in
-    printf "%a@." TreeAutomaton.pp ta;
+    (* printf "%a@." TreeAutomaton.pp ta; *)
     let t  = load_tree Sys.argv.(2) in
-    printf "%a@." Tree.pp t;
+    (* printf "%a@." Tree.pp t; *)
     let tmap = map_of_binary_tree t in
-    let r_bu = TreeAutomaton.validate_bu ta tmap "" in
-    printf "%a@." pp_string_set r_bu;
     let r_opt = TreeAutomaton.validate_opt ta tmap "" ta.initial in
-    printf "%a@." pp_string_set r_opt
+    (* printf "%a@." pp_string_set r_opt *)
+    if StringSet.mem "accept" r_opt then printf "Valide@."
+    else printf "Invalide@."
   end
   else begin
     if Array.length Sys.argv > 1 then
